e3ccf41393b8869a2cb6b63eb068c262178bfcad
K.Kimura <dotnsf@gmail.com>
2019-09-28 12:35:54 +0900

    setting.js added.

---
+++ settings.js
@@ -0,0 +1,8 @@
+//. settings.js
+exports.redis_hostname = 'hostname';
+exports.redis_port = 6379;
+
+exports.twitter_consumer_key = '';
+exports.twitter_consumer_secret = '';
+
+exports.superSecret = 'hostname.js';
